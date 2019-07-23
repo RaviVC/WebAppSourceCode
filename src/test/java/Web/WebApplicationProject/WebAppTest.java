476821e5718e1e9f8c11f159027ff897f3eb80c7
RaviVC <ravivc197@gmail.com>
2019-07-23 19:35:26 +0530

    WebChanges

---
+++ src/test/java/Web/WebApplicationProject/WebAppTest.java
@@ -0,0 +1,12 @@
+package Web.WebApplicationProject;
+
+import org.testng.annotations.Test;
+
+public class WebAppTest {
+	
+	@Test
+	public void WebApp(){
+		System.out.println("WebApplicationLaunched");
+	}
+
+}
